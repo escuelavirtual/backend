74617ec36df6e755b25880b2c9595d6df3826985
jonathan0126 <39790533+jonathan0126@users.noreply.github.com>
2020-08-01 20:54:28 -0500

    rutas y middlewares para agregar un usuario

---
+++ controller/userController.js
@@ -0,0 +1,28 @@
+const User = require("../model/userModel");
+const {validationResult} = require("express-validator");
+
+
+//create user
+
+exports.createUser = async (req, res) => {
+
+    //we check if there is an error with the request
+    const errores = validationResult(req);
+    if(!errores){
+        return res.status(400).json({errores:errores.array()});
+    }
+
+    try{
+        //create user with req.body
+        const user = new User(req.body);
+
+        //save user in the database
+        user.save()
+
+        //return a json with the user
+        res.json(user);
+    }catch(error){
+        console.log(error);
+        res.status(500).json({msg:"There was a mistake"});
+    }
+}
