1e506302b51a007153c7c8ab89e9d0d75df53f66
tbvescio <43358562+tbvescio@users.noreply.github.com>
2020-08-04 16:49:39 -0300

    basic auth

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,31 @@
 const Usuario = require("../models/Usuario");
 const bcryptjs = require("bcryptjs");
-const {validationResult} = require("express-validator");
-const jwt = require('jsonwebtoken');
+const { validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
 
-exports.authenticateUser = async (req, res) => {
+exports.register = (req, res) => {
+  //validate errors on request
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
 
-    //validate errors on request
-    const errors = validationResult(req)
-    if(!errors.isEmpty()){return res.status(400).json({errors : errors.array()})}
+  //TODO:
+  //DB LOGIC
+  //BCRYPT LOGIC
+};
 
+exports.login = (req, res, next) => {
+  //validate errors on request
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
 
-}
+  const email = req.body.email;
+  const password = req.body.password;
+
+  //TODO:
+  //DB logic
+  //JWT LOGIC
+};
--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const router = express.Router();
-const authController = require('../controllers/authController');
-const {check} = require('express-validator');
-const auth = require('../middlewer/auth')
+const authController = require("../controllers/authController");
+const { check } = require("express-validator");
+const auth = require("../middlewer/auth");
 
 //login
-router.post("/",[
-        check("email", "Email required"),
-        check("password","password required")
-],
-    authController.authenticateUser
-)
+router.post(
+  "/register",
+  [check("email", "Email required"), check("password", "password required")],
+  authController.register
+);
 
+router.post(
+  "/login",
+  [check("email", "Email required"), check("password", "password required")],
+  authController.register
+);
 
 module.exports = router;
