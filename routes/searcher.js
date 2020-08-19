1b00f4554ce4a66dc719e18e1f6b815ceb1484d3
lalo1234321 <edominguezc@toluca.tecnm.mx>
2020-08-19 12:56:41 -0500

    Important changes
    -The course.js and auth.js were deleted because we didnt used and Dash suggest to erase them
    -Chages in the index.js,in the importation routes
    -The verify method of the authController.js was moved to the is-auth.js in the middlewares  carpet
    -The  previous was executed beacause the verifyToken act  as a middleware in some requests
    -The config/config.js was modified adding the environment variables
    -The routes/searcher.js was modified  because verifytoken now is on the middlewares carpet

--- routes/searcher.js
+++ routes/searcher.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const is_auth = require("../middlewares/is-auth");
+const {verifyToken} = require("../middlewares/is-auth");
 const searchController = require("../controllers/searchController");
 const searchCategoryController = require("../controllers/searcherCategoryController");
 
-router.get("/search", is_auth, searchController.searcher);
-router.get("/search/category/:category", is_auth, searchCategoryController.searcher)
+router.get("/search", verifyToken, searchController.searcher);
+router.get("/search/category/:category", verifyToken, searchCategoryController.searcher)
 
 module.exports = router;
