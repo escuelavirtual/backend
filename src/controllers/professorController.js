df7f40e319127eb168a057627af041c0d20a94e3
Alejandro <snackbarchat@gmail.com>
2020-09-03 04:16:20 -0300

    refactor: add studentsController and fix some issues

--- src/controllers/professorController.js
+++ src/controllers/professorController.js
@@ -1,58 +1,58 @@
+const e = require("debug")("error:data")
 const bcrypt = require('bcrypt');
 const Professor = require('../models/professor');
 const User = require('../models/user');
 
-exports.getProfessors = async(req,res)=>{
-    try{
-        const professor=await Professor.findAll();
-        res.status(200).json({
-            ok:true,
-            professor
+exports.getProfessors = async (req, res) => {
+    try {
+        const professor = await Professor.findAll();
+        res.status(200).json({    
+            professors: professor
         });
 
-    }catch(error){
+    } catch (error) {
         res.status(500).json({
-            ok:false,
-            message:'We couldn´t find the professors'
+           
+            message: 'We couldn´t find the professors'
         })
     }
-    
+
 }
 
-exports.createProfessor=async(req,res)=>{
-    try{
-        if(req.body.email==null||req.body.password==null||req.body.firstname==null||req.body.lastname==null){
-            return res.status(400).json({
-                ok:false,
-                message:'Bad request'
-            })
+exports.createProfessor = async (req, res) => {
+    try {
+        if (req.body.email == null || req.body.password == null || req.body.firstname == null || req.body.lastname == null) {
+            return res.status(400).json({ message: 'Bad request' })
         }
-        const user=await User.create({
-        firstname:req.body.firstname,
-        lastname:req.body.lastname,
-        email:req.body.email,
-        profile_image:req.body.profile_image,
-        password:bcrypt.hashSync(req.body.password,10)
+
+        const user = await User.create({
+            firstname: req.body.firstname,
+            lastname: req.body.lastname,
+            email: req.body.email,
+            password: bcrypt.hashSync(req.body.password, 10)
         });
-        const id=user.id;
-        const professor=await Professor.create({
-           userId:id,
-           valuation:req.body.valuation
+
+        const professor = await Professor.create({
+            userId: user.id
         });
-         
+
         res.status(201).json({
-            ok:true,
-            message:'User created with professsor rol',
-            user,
-            professor
+            message: 'Professor was created',
+            data: {
+                id: professor.id,
+                userId: user.id,
+                firstname: user.firstname,
+                lastname: user.lastname,
+                email: user.email,
+                createdAt: user.createdAt,
+            }
         })
-    }catch(error){
-       
-        res.status(500).json({
-            ok:false,
-            message:'Internal error'
-        })
+    } catch (err) {
+
+        e(err.message);
+        res.status(500).json({ message: err.message })
+        
     }
-    
+
 
 }
