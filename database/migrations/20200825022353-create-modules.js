0f7b1f7788eace09031c20b1deb7fc80e1c8071c
Alejandro <snackbarchat@gmail.com>
2020-09-03 04:13:26 -0300

    fix: fix some migrations

--- database/migrations/20200825022353-create-modules.js
+++ database/migrations/20200825022353-create-modules.js
@@ -1,37 +1,37 @@
 'use strict';
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('modules', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-     courseId: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        references: { model: 'courses', key: 'id' }
-      },
-      title:{
-        type:Sequelize.STRING,
-        allowNull: false
-      },
-      description:{
-        type:Sequelize.TEXT,               
-         allowNull: false
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('modules');
-  }
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.createTable('modules', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            courseId: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                references: { model: 'courses', key: 'id' }
+            },
+            title: {
+                type: Sequelize.STRING,
+                allowNull: false
+            },
+            description: {
+                type: Sequelize.TEXT,
+                allowNull: false
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    down: async (queryInterface, Sequelize) => {
+        await queryInterface.dropTable('modules');
+    }
 };
