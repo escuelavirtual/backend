b263aad28bf1c990747e5a818d50e0bacfd57227
Alejandro <snackbarchat@gmail.com>
2020-09-07 01:31:53 -0300

    feature: validation in routes

--- src/api/routes/courses.js
+++ src/api/routes/courses.js
@@ -1,12 +1,14 @@
 const { Router } = require('express');
 const router = Router();
-const CoursesController =require('../../controllers/coursesController');
-const {verifyToken}=require('../middlewares/is-auth');
+const CoursesController = require('../../controllers/coursesController');
+const { verifyToken } = require('../middlewares/is-auth');
+const { validator } = require('../middlewares/validator');
+const CourseService = require('../../services/courseService');
 
 
-router.post('/',verifyToken, CoursesController.createCourse);
-router.delete('/:id',verifyToken, CoursesController.deleteCourse);
-router.put('/:id',verifyToken, CoursesController.updateCourse);
+router.post('/', [verifyToken, validator(CourseService.validate())], CoursesController.createCourse);
+router.delete('/:id', verifyToken, CoursesController.deleteCourse);
+router.put('/:id', verifyToken, CoursesController.updateCourse);
 router.get('/', CoursesController.searchCourse);
 
 module.exports = router;
--- src/api/routes/professors.js
+++ src/api/routes/professors.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const ProfessorController = require('../../controllers/professorController');
+const { validator } = require('../middlewares/validator');
+const UserService = require('../../services/userService');
 
 router.get('/', ProfessorController.getProfessors);
-router.post('/', ProfessorController.createProfessor);
+router.post('/', validator(UserService.validate()), ProfessorController.createProfessor);
 
 module.exports = router;
